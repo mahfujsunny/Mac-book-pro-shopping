16fe9c56542240b03f2b36f6afa1465071f4c3d8
mahfujsunny <mahfujsunny.info@gmail.com>
2021-08-21 01:49:34 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+////////// Memory section //////////
+
+const builtInMemory = document.getElementById('8gb-memory');
+const extraMemory = document.getElementById('16gb-memory');
+
+const memoryCost = document.getElementById('extra-memory')
+
+
